906f3e6a6952fa0d8aa1e524cb209f64a34490a2
Yahir2525 <yahir.velazquez5481@alumnos.udg.mx>
2024-09-22 01:30:05 -0600

    Probando de nuevo

--- src/app/components/Derechos.jsx
+++ src/app/components/Derechos.jsx
@@ -4,7 +4,7 @@
 function Derechos (){
     return (
     <>
-        <h1 className="py-2 text-black text-lg font-bold flex items-center justify-center">© Rentas Yamora 2024 Todos los derechos reservados
+        <h1 className="py-2 text-black text-lg font-bold flex items-center justify-center">© Rentas Yamora 2024 Todos los derechos reservados gracias
         </h1>
     </>
     )
