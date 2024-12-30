ccdc7bb7c8f9d302bc5facd9451e6be3211c6734
javier <diego.mora5781@alumnos.udg.mx>
2024-12-29 22:19:21 -0600

    base layout

--- yamora/app/properties/[id]/page.jsx
+++ yamora/app/properties/[id]/page.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+'use client';
+import { SearchParamsContext } from "next/dist/shared/lib/hooks-client-context.shared-runtime";
+import { useParams, useRouter, useSearchParams, 
+  usePathname
+ } from "next/navigation"
 
 const PropertyAddPage = () => {
+  const router = useRouter();
+  console.log('sis')
+  const {id} = useParams();
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+
+  const name = searchParams.get('name')
   return (
-    <div>PropertyAddPage</div>
+    <div>
+      <button onClick={ ()=> router.push('/')} className="bg-blue-400">
+        Página Principal {id} {name} {pathname}
+        </button>
+    </div>
   )
 }
 
