47ed2a7d97ad9ce4a6c33e53a65cf3395f2b75d4
javier <diego.mora5781@alumnos.udg.mx>
2024-12-29 18:47:15 -0600

    first commit

---
+++ yamora/app/layout.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import '@/assets/styles/globals.css'
+import HomePage from './page'
+//import HomePage from './page'
+export const metadata = {
+    title: 'PropertyPulse | Find the perfect rental',
+    description: 'Find your dream rental property',
+    keywords: 'rental, find rentals, find properties',
+    equisde: 'rental, find rentals, find properties',
+}
+
+
+const MainLayout = ( {children} ) => {
+  return (
+    <html lang='en'>
+    <body>
+         <div>
+            {children}
+          </div>    
+    </body>
+    </html>
+    
+    
+  )
+}
+
+export default MainLayout
---
+++ yamora/app/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1 className="text-3xl">Bienvenido!</h1>
+      <a href="/properties" className="href"></a>
+    </div>
+  )
+}
+
+export default HomePage;
