5ac132dfd23b35f73cd290b2f7caf8687b131620
Vamezyah <cesar.vargas9379@alumnos.udg.mx>
2025-05-15 20:29:53 -0600

    Inicio de sesion google

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,20 +1,30 @@
 'use client'
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import logo from '@/assets/images/logo-white.png';
 import profileDefault from '@/assets/images/profile.png';
 import {FaGoogle} from 'react-icons/fa';
+import{signIn, signOut, useSession, getProviders} from 'next-auth/react';
 
 const Navbar = () => {
+
+    const{data: session} = useSession();
 
     const pathname = usePathname();
 
     const [isMobileMenuOpen,setIsMobileMenuOpen] = useState(false)
     const [isProfileMenuOpen,setIsProfileMenuOpen] = useState(false)
+    const [providers, setProviders] = useState(null);
 
-    const [isLoggedIn, setIsLoggedIn] = useState(true);
+    useEffect(()=>{
+      const setAuthProviders = async ()=> {
+        const res = await getProviders();
+        setProviders(res);
+      }
+      setAuthProviders();
+    },[]);
 
     return ( <nav className="bg-purple-700 border-b border-purple-500">
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
@@ -63,7 +73,7 @@ const Navbar = () => {
                     href="/properties"
                     className={`${pathname === '/properties' ? 'bg-black' : ''} text-white hover:bg-gray-900 hover: text-white rounded-md px-3 py-2`}
                     >Propiedades</Link>
-                  {isLoggedIn && (
+                  {session && (
                   <Link
                     href="/properties/add"
                     className={`${pathname === '/properties/add' ? 'bg-black' : ''} text-white hover:bg-gray-900 hover: text-white rounded-md px-3 py-2`}
@@ -75,22 +85,25 @@ const Navbar = () => {
             
             {/* <!-- Right Side Menu (Logged Out) --> */}
             {
-              !isLoggedIn && (
+              !session && (
 
             <div className="hidden md:block md:ml-6">
               <div className="flex items-center">
-                <button
-                  className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                >
-                  <FaGoogle className='text-white mr-2'/>
-                  <span>Iniciar sesión o Registrarse</span>
-                </button>
+                {providers && Object.values(providers).map((provider, index)=>(
+                     <button key={index}
+                        onClick={ () => signIn(provider.id)}
+                        className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                      >
+                        <FaGoogle className='text-white mr-2'/>
+                        <span>Iniciar sesión o Registrarse</span>
+                   </button>
+                ))}
               </div>
             </div>
               )}
             {/* <!-- Right Side Menu (Logged In) --> */}
             {
-              isLoggedIn && (
+              session && (
             
             <div
               className="absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-6 md:pr-0"
@@ -216,14 +229,14 @@ const Navbar = () => {
               href="/properties"
               className={`${pathname === '/properties' ? 'bg-black' : ''} text-white block rounded-md px-3 py-2 text-base font-medium`}
               >Propiedades</Link>
-            { isLoggedIn && (
+            { session && (
             <Link
               href="/properties/add"
               className={`${pathname === '/properties/add' ? 'bg-black' : ''} text-white block rounded-md px-3 py-2 text-base font-medium`}
               >Añadir propiedad</Link>
             )}
             {
-              !isLoggedIn && (
+              !session && (
             <button
               className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-5"
             >
