62b92354da0d11f61c7a5569b3c8d80dec134098
Diego Mora <mora.diego@ibm.com>
2025-08-17 14:12:02 -0600

    search functionality added

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,3 +1,4 @@
+import PropertySearchForm from './PropertySearchForm.jsx'
 const Hero = () => {
     return (<section className="bg-purple-700 py-20 mb-4">
         <div
@@ -14,41 +15,8 @@ const Hero = () => {
             </p>
           </div>
           {/*<!-- Form Component -->*/}
-          <form
-            className="mt-3 mx-auto max-w-2xl w-full flex flex-col md:flex-row items-center"
-          >
-            <div className="w-full md:w-3/5 md:pr-2 mb-4 md:mb-0">
-              <label htmlFor="location" className="sr-only">Location</label>
-              <input
-                type="text"
-                id="location"
-                placeholder="Ubicación, calle, ciudad, código postal..."
-                className="w-full px-4 py-3 rounded-lg bg-white text-gray-800 focus:outline-none focus:ring focus:ring-purple-500"
-              />
-            </div>
-            <div className="w-full md:w-2/5 md:pl-2">
-              <label htmlFor="property-type" className="sr-only">Property Type</label>
-              <select
-                id="property-type"
-                className="w-full px-4 py-3 rounded-lg bg-white text-gray-800 focus:outline-none focus:ring focus:ring-purple-500"
-              >
-                <option value="All">Todas</option>
-                <option value="Apartment">Departamento</option>
-                <option value="Studio">Estudio</option>
-                <option value="Condo">Condominio</option>
-                <option value="House">Casa</option>
-                <option value="Cabin Or Cottage">Cabina</option>
-                <option value="Room">Cuarto</option>
-                <option value="Other">Otro</option>
-              </select>
-            </div>
-            <button
-              type="submit"
-              className="md:ml-4 mt-4 md:mt-0 w-full md:w-auto px-6 py-3 rounded-lg bg-purple-500 text-white hover:bg-purple-600 focus:outline-none focus:ring focus:ring-purple-500"
-            >
-              Buscar
-            </button>
-          </form>
+        <PropertySearchForm/>
+          
         </div>
       </section>
   );
