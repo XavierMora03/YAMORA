c7da0b5b5b7bfa68d617dba2e55f635fc68cc7e9
Diego Mora <mora.diego@ibm.com>
2025-10-13 11:49:27 -0600

    test db timeout

--- config/database.js
+++ config/database.js
@@ -1,25 +1,27 @@
 import mongoose from "mongoose";
 
-let connected = false;
+   let connected = false;
 
-const connectDB = async()=>{
-    mongoose.set('strictQuery',true);
+   const connectDB = async () => {
+     mongoose.set('strictQuery', true);
 
-    //if database is already connected, not connect again
+     if (connected) {
+       console.log('MongoDB is already connected');
+       return;
+     }
 
-    if(connected){
-        console.log('MongoDB is already connected');
-        return;
-    }
+     try {
+       await mongoose.connect(process.env.MONGODB_URI, {
+         serverSelectionTimeoutMS: 5000,
+         socketTimeoutMS: 45000,
+         connectTimeoutMS: 10000,
+       });
+       connected = true;
+       console.log('MongoDB connected');
+     } catch (error) {
+       console.error('MongoDB connection error:', error);
+       throw error;
+     }
+   };
 
-    //conect to mongoDB
-
-    try{
-        await mongoose.connect(process.env.MONGODB_URI);
-        connected = true;
-    } catch(error){
-        console.log(error);
-    }
-};
-
-export default connectDB;
+   export default connectDB;
